6453acc6d008a0d4321350a38f831ee0d27a056c
jalb <jalburquerque@symexo.com>
2023-12-26 14:44:10 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using LabCSharp.Async;
+
+Console.WriteLine("Lab C#");
+
+var separation = new String('-', 25);
+
+// Async
+// https://learn.microsoft.com/fr-fr/dotnet/csharp/asynchronous-programming/
+
+Console.WriteLine(separation);
+Console.WriteLine("Async - Exemple 1 - SyncProgram");
+Console.WriteLine(separation);
+SyncProgram.run();
+
+Console.WriteLine(separation);
+Console.WriteLine("Async - Exemple 1 - AsyncProgram");
+Console.WriteLine(separation);
+await AsyncProgram.run();
+
+Console.WriteLine();
+
